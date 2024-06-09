1fdb1b74834d62867b239373b6480e869170033b
AndreyJavaEdu <scream123456@yandex.ru>
2024-06-10 01:43:35 +0500

    Создали Эвент и отправили его в топик Кафки

--- src/main/java/com/kamenskiy/io/productmicroservice/service/ProductServiceImpl.java
+++ src/main/java/com/kamenskiy/io/productmicroservice/service/ProductServiceImpl.java
@@ -1,12 +1,42 @@
 package com.kamenskiy.io.productmicroservice.service;
 
 import com.kamenskiy.io.productmicroservice.service.dto.CreateProductDto;
+import com.kamenskiy.io.productmicroservice.service.event.ProductCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+   private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    private KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
+    public ProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Override
     public String createProduct(CreateProductDto productDto) {
-        return "";
+        //TODO save to DB
+        String productId = UUID.randomUUID().toString();
+
+        ProductCreatedEvent event = new ProductCreatedEvent(productId, productDto.getTitle(), productDto.getPrice(),
+                productDto.getQuantity());
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> future = kafkaTemplate
+                .send("product-created-events-topic", productId, event);
+
+        future.whenComplete((result, exception) ->{
+           if (exception != null) {
+               LOGGER.error("Failed to send message: {}", exception.getMessage());
+           } else
+                LOGGER.info("Message send successfully: {}", result.getRecordMetadata());
+        });
+        LOGGER.info("Return productId: {}", productId);
+        return productId;
     }
 }
